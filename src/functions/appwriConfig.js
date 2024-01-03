2f0c6986e54fa1cc86f66715e68b6ee8c4327bbb
kassycodes13 <ichokusomtoo12@gmail.com>
2024-01-03 04:09:03 +0100

    main

--- src/functions/appwriConfig.js
+++ src/functions/appwriConfig.js
@@ -1,33 +1,46 @@
-import {
-  Client,
-  Account,
-  Databases,
-  Avatars,
-  Storage,
-  Query,
-  Functions,
-} from "appwrite";
+// import {
+//   Client,
+//   Account,
+//   Databases,
+//   Avatars,
+//   Storage,
+//   Query,
+//   Functions,
+// } from "appwrite";
 
-const client = new Client();
-export const account = new Account(client);
-export const avatars = new Avatars(client);
-export { ID } from "appwrite";
-export const databases = new Databases(client);
-export const storageBucket = new Storage(client);
-export const query = new Query(client);
-export const appwriteFunction = new Functions(client);
+// const client = new Client();
+// export const account = new Account(client);
+// export const avatars = new Avatars(client);
+// export { ID } from "appwrite";
+// export const databases = new Databases(client);
+// export const storageBucket = new Storage(client);
+// export const query = new Query(client);
+// export const appwriteFunction = new Functions(client);
+
+// // client
+// //   .setEndpoint(process.env.VITE_APPWRITE_ENDPOINT)
+// //   .setProject(process.env.VITE_PROJECT_ID);
+
+// // appwriConfig.js
+// console.log("Before setEndpoint:", process.env.VITE_APPWRITE_ENDPOINT);
 
 // client
-//   .setEndpoint(process.env.VITE_APPWRITE_ENDPOINT)
+//   .setEndpoint("https://cloud.appwrite.io/v1")
 //   .setProject(process.env.VITE_PROJECT_ID);
 
-// appwriConfig.js
-console.log("Before setEndpoint:", process.env.VITE_APPWRITE_ENDPOINT);
+// console.log("After setEndpoint:", client.config.endpoint);
 
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.VITE_PROJECT_ID);
+// // ... rest of your code ...
 
-console.log("After setEndpoint:", client.config.endpoint);
-
-// ... rest of your code ...
+export default async ({ req, res, log, error }) => {
+  if (req.method === "GET") {
+    return res.send("This was a GET request");
+  }
+  if (req.method === "POST") {
+    return res.json({
+      sentData: req.body,
+      DOGENV: process.env.DOG_NAME,
+      CATENV: process.env.CAT_NAME,
+    });
+  }
+};
